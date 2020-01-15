b3d157272043828257c9adc081b7aabe9fe87b82
SW <dagedieb@gmail.com>
2020-01-15 21:32:05 +0100

    config

--- config.py
+++ config.py
@@ -22,24 +22,24 @@ science_map="../ScienceMap/GUI.py"
 science_map_venv="../ScienceMap/venv/bin/activate"
 science_map_csv="../ScienceMap/manual_corpus/relations.csv"
 
-ampligraph_working_dir="../allennlp_vs_ampligraph/"
-ampligraph_venv="../allennlp_vs_ampligraph/venv/bin/activate"
-ampligraph="../allennlp_vs_ampligraph/csv_ampligraph.py"
+ampligraph_working_dir="../KnowledgeScience/"
+ampligraph_venv="../KnowledgeScience/venv/bin/activate"
+ampligraph="../KnowledgeScience/csv_ampligraph.py"
 ampligraph_coords="CONSTRASTSUBJECT"
 
-train_venv_python = "/roedel/home/finn/ai-difference/venv/bin/activate"
-train_path= "/roedel/home/finn/ai-difference/Distinctiopus4"
-train_script= "/roedel/home/finn//ai-difference/Distinctiopus4/do/train_multi_corpus.py"
+train_venv_python = "../Distinctiopus4/venv/bin/activate"
+train_path= "../Distinctiopus4"
+train_script= "../Distinctiopus4/do/train_multi_corpus.py"
 train_log = "train.log"
-allennlp_config = "/roedel/home/finn//ai-difference/Distinctiopus4/experiment_configs/elmo_lstm3_feedforward4_crf_straight_fitter.config"
+allennlp_config = "../Distinctiopus4/experiment_configs/elmo_lstm3_feedforward4_crf_straight_fitter.config"
 
-dist_model_path_first = "/roedel/home/finn/ai-difference/Distinctiopus4/output/first_./experiment_configs/{config}/model.tar.gz".format(config=allennlp_config)
-cc_model_path_first   = "/roedel/home/finn/CorpusCook/server/models/model_first.tar.gz"
-dist_model_path_over  = "/roedel/home/finn/ai-difference/Distinctiopus4/output/over_./experiment_configs/{config}/model.tar.gz".format(config=allennlp_config)
-cc_model_path_over    = "/roedel/home/finn/CorpusCook/server/models/model_over.tar.gz"
+dist_model_path_first = "../Distinctiopus4/output/first_./experiment_configs/{config}/model.tar.gz".format(config=allennlp_config)
+cc_model_path_first   = "../CorpusCook/server/models/model_first.tar.gz"
+dist_model_path_over  = "../Distinctiopus4/output/over_./experiment_configs/{config}/model.tar.gz".format(config=allennlp_config)
+cc_model_path_over    = "../CorpusCook/server/models/model_over.tar.gz"
 
-all_coordinates="/home/stefan/PycharmProjects/allennlp_vs_ampligraph/knowledge_graph_coords/knowledge_graph_3d_choords.csv"
-ke_path=  "/home/stefan/PycharmProjects/allennlp_vs_ampligraph/knowledge_graph_coords/tsne_clusters_mean_points.csv"
-ke_colors="/home/stefan/PycharmProjects/allennlp_vs_ampligraph/knowledge_graph_coords/kn_clusters_mean_points.csv"
-hal = '"/home/stefan/IdeaProjects/hal/target/hal-1-jar-with-dependencies.jar"'
-video_dir = '/home/stefan/IdeaProjects/view_control_web/WebContent/resources/media/'
+all_coordinates="../KnowledgeScience/knowledge_graph_coords/knowledge_graph_3d_choords.csv"
+ke_path=  "../KnowledgeScience/knowledge_graph_coords/tsne_clusters_mean_points.csv"
+ke_colors="../KnowledgeScience/knowledge_graph_coords/kn_clusters_mean_points.csv"
+hal = '"../hal/target/hal-1-jar-with-dependencies.jar"'
+video_dir = '../view_control_web/WebContent/resources/media/'
